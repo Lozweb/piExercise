f5b0453c180c7aa3d46b0faa5e45358c195893b7
BuildTools <aelozweb@gmail.com>
2023-10-29 14:17:55 +0100

    test file

---
+++ parts/test.py
@@ -0,0 +1,15 @@
+import servo_moteur as sm
+
+if __name__ == '__main__':
+    print('Program is starting...')
+    sm.setup()
+try:
+
+    while True:
+
+        angle = input("Entrez l'angle")
+        sm.servo_write(angle)
+
+
+except KeyboardInterrupt:
+    sm.destroy()
