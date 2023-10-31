c10017f3bb66a899ac9a762a728284758fce796e
BuildTools <aelozweb@gmail.com>
2023-10-31 21:10:11 +0100

    add other debug value

--- parts/test.py
+++ parts/test.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
             manette.controler.axis_l.when_moved = manette.on_axis_l_moved
             manette.controler.axis_r.when_moved = manette.on_axis_l_moved
             target_pos = round(manette.current_ly_pos + 5)
+            print(manette.current_ly_pos)
             print(target_pos)
             servo.servo_write(target_pos)
             time.sleep(0.001)
