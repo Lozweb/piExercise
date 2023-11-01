3cb0e8da54e23d3f27e7bab53f93e5c15fb00b6d
BuildTools <aelozweb@gmail.com>
2023-11-01 16:04:29 +0100

    add debug print

--- parts/motor_power.py
+++ parts/motor_power.py
@@ -1,39 +1,45 @@
-from time import sleep
 import RPi.GPIO as GPIO
+from time import sleep
 
-# Modifiez pour mettre les pins sur lesquels sont branchés les entrées de la L293D
-Motor1E = 22
-Motor1A = 18
-Motor1B = 16
+# Pins for Motor Driver Inputs
+Motor1A = 24
+Motor1B = 23
+Motor1E = 25
 
 
-try:
-
-    # Configure les pins
-    GPIO.setmode(GPIO.BCM)
-
-    GPIO.setup(Motor1E, GPIO.OUT)
-    GPIO.setup(Motor1A, GPIO.OUT)
+def setup():
+    GPIO.setmode(GPIO.BCM)  # GPIO Numbering
+    GPIO.setup(Motor1A, GPIO.OUT)  # All pins as Outputs
     GPIO.setup(Motor1B, GPIO.OUT)
+    GPIO.setup(Motor1E, GPIO.OUT)
 
-    # AVANCE
+
+def loop():
+    # Going forwards
+    print("forward")
     GPIO.output(Motor1A, GPIO.HIGH)
     GPIO.output(Motor1B, GPIO.LOW)
     GPIO.output(Motor1E, GPIO.HIGH)
-    print("forward")
-    sleep(5)
 
+    sleep(5)
+    # Going backwards
+    print("backward")
     GPIO.output(Motor1A, GPIO.LOW)
     GPIO.output(Motor1B, GPIO.HIGH)
     GPIO.output(Motor1E, GPIO.HIGH)
-    print("backward")
+
     sleep(5)
+    # Stop
+    GPIO.output(Motor1E, GPIO.LOW)
 
-except KeyboardInterrupt:
-    pass
 
-except:
+def destroy():
     GPIO.cleanup()
-    raise
 
-GPIO.cleanup()
+
+if __name__ == '__main__':  # Program start from here
+    setup()
+try:
+    loop()
+except KeyboardInterrupt:
+    destroy()
