1e82e872103a1f18d5a68bd6fc0b5f5d8a18bb7c
BuildTools <aelozweb@gmail.com>
2023-11-04 21:38:25 +0100

    add manette to commande direction

--- parts/manette.py
+++ parts/manette.py
@@ -5,7 +5,7 @@ class Manette:
     def __init__(self, port):
         self.port = port
         self.controler = Xbox360Controller(self.port, axis_threshold=0.2)
-        self.l_dead_zone_min, self.l_dead_zone_max = (-0.5, 0.5)
+        self.l_dead_zone_min, self.l_dead_zone_max = (-0.8, 0.8)
         self.lx_pos = 0
         self.ly_pos = 0
         self.trig_lt_pos = 0
